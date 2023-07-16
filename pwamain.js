1be326b90cbd1e4c21eb62ce6831e666288c33d5
mdjoynalabedin61 <123541614+mdjoynalabedin61@users.noreply.github.com>
2023-07-16 15:57:07 +0600

    Add files via upload

---
+++ pwamain.js
@@ -0,0 +1,85 @@
+/*addEventListener("beforeinstallprompt", (event) => {});
+
+onbeforeinstallprompt = (event) => {};
+
+let installPrompt = null;
+const installButton = document.querySelector("#install");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  installPrompt = event;
+  installButton.removeAttribute("hidden");
+});
+
+
+installButton.addEventListener("click", async () => {
+  if (!installPrompt) {
+    return;
+  }
+  const result = await installPrompt.prompt();
+  console.log(`Install prompt was: ${result.outcome}`);
+  installPrompt = null;
+  installButton.setAttribute("hidden", "");
+});
+*/
+
+// main.js
+
+let installPrompt = null;
+const installButton = document.querySelector("#install");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  installPrompt = event;
+  installButton.removeAttribute("hidden");
+});
+
+// main.js
+
+installButton.addEventListener("click", async () => {
+  if (!installPrompt) {
+    return;
+  }
+  const result = await installPrompt.prompt();
+  console.log(`Install prompt was: ${result.outcome}`);
+  disableInAppInstallPrompt();
+});
+
+function disableInAppInstallPrompt() {
+  installPrompt = null;
+  installButton.setAttribute("hidden", "");
+}
+
+
+// main.js
+
+window.addEventListener("appinstalled", () => {
+  disableInAppInstallPrompt();
+});
+
+function disableInAppInstallPrompt() {
+  installPrompt = null;
+  installButton.setAttribute("hidden", "");
+}
+// main.js
+
+window.addEventListener("appinstalled", () => {
+  disableInAppInstallPrompt();
+});
+
+function disableInAppInstallPrompt() {
+  installPrompt = null;
+  installButton.setAttribute("hidden", "");
+}
+
+window.addEventListener("beforeinstallprompt", async (event) => {
+  const relatedApps = await navigator.getInstalledRelatedApps();
+
+  // Search for a specific installed platform-specific app
+  const psApp = relatedApps.find((app) => app.id === "com.example.myapp");
+
+  if (psApp) {
+    event.preventDefault();
+    // Update UI as appropriate
+  }
+});
